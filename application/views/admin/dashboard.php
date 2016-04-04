19ec066601ea95171324a781570a3337bcce84dc
Katrina <blueyed_qt@hotmail.com>
2016-04-03 18:26:33 -0700

    Added minor comments to come back to

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,5 +1,8 @@
 			<div class="content">
  				<h1>Welcome to the Admin Dashboard!</h1>
+<!-- ## Display Admin Name and Welcome Message ## -->
+<!-- ## Change Modal Text ## -->
+<!-- ## Upload, view, select PDF for report! ## -->
 			</div>
 		</div><!-- closing container -->
 	</body>
